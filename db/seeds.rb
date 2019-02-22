4983f569005a1818282f7e0e8645520bddf89642
Anna Pettersen <aopettersen@gmail.com>
2019-02-22 12:09:29 +0000

    Fixed controllers and routes - cocktails & doses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+Cocktail.create(name: "cocktail 1")
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+ingredient = JSON.parse(user_serialized)
+
+ingredient['drinks'].each do |ingred|
+  Ingredient.create(name: ingred)
+end
